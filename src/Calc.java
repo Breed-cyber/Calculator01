4699009180416f3689146bf3933fff5e65e28e7a
Breed-cyber <68813232+Breed-cyber@users.noreply.github.com>
2020-08-08 23:45:50 +0300

    калькулятор добавилл ввод

--- src/Calc.java
+++ src/Calc.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
-        Scanner
-
+        Scanner k = new Scanner(System.in);
+        System.out.println("Число yyy1");
+        System.out.println("Число 1");
+        int numb1 = Integer.parseInt(k.next());//числа в строку//
+        System.out.println("Ведите символ");
+        char seign = k.next().charAt(0);
+        System.out.println("Число2");
+        int num2 = Integer.parseInt(k.next());
+        System.out.println(numb1 + "" + seign + "" + num2);
     }
 }
